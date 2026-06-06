4e0fd65901d8cfa400d3e4165086ad589a4fdcd1
MehmetErenTopal <topalmehmeteren61ts@gmail.com>
2026-06-06 22:35:10 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  // GitHub Pages üzerinde subfolder (alt klasör) olarak çalıştığı için depo adını ekliyoruz.
+  plugins: [react(), tailwindcss()],
   base: '/lego-ai/',
 })
